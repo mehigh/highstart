e875f7448d10cac31d1ff6c0630326865f440498
Mike <mike@xhtmlized.com>
2014-08-09 17:32:41 +0300

    SASS: Basic CSS utils
    - Added placeholder typography styles & em mixin
    - Made border-box box-sizing default
    - Added Utility classes for standard WP editor
    - Added utility classes for resposive videos

--- html/index.php
+++ html/index.php
@@ -5,18 +5,61 @@
 <hr>
 <h2>Typography</h2>
 
-<h1>Heading 1</h1>
-<p>Lorem ipsum dolor sit amet.</p>
+	<article>
 
-<h2>Heading 2</h2>
-<p>Consectetur adipisicing elit veritatis minus.</p>
+		<h1>Heading1</h1>
+		<p>Base 16px/22px</p>
+		<h2>Heading2</h2>
+		<p>paragraph</p>
+		<h3>Heading3</h3>
+		<p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+		<h4>Heading4</h4>
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum:</p>
+
+		<ul>
+			<li>UL 16px/22px</li>
+			<li>consectetur adipisicing elit
+				<ol>
+					<li>OL</li>
+					<li>sublist</li>
+				</ol>
+			</li>
+			<li>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</li>
+		</ul>
+
+		<h5>Heading5</h5>
+		<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<h6>Heading6</h6>
+		<p>Ut enim ad minim veniam:</p>
+
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque similique, maxime, saepe asperiores, sint eveniet error rerum qui a porro culpa numquam velit ea quam, nihil animi doloremque. Adipisci, atque!</p>
+		<ol>
+			<li>OL quis nostrud exercitation ullamco
+				<ul>
+					<li>UL unordered</li>
+					<li>sublist</li>
+				</ul>
+			</li>
+			<li>laboris nisi ut aliquip ex ea commodo consequat</li>
+			<li>duis aute irure dolor in reprehenderit in</li>
+			<li>voluptate velit esse cillum dolore eu fugiat nulla pariatur</li>
+		</ol>
+
+		<p>eg.</p>
+		<blockquote><p>Some quoted text in here.</p></blockquote>
+		<p><img src="http://placehold.it/200x200" alt="" class="alignleft" />And some text after the quote.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p><img src="http://placehold.it/200x200" alt="" class="alignright" />Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. <img src="http://placehold.it/200x200" alt="" class="aligncenter" /></p>
+
+	</article>
 
 <hr>
 <h2>Utility Classes</h2>
 
-<div class="visible-small"><strong>.visible-small</strong> - Visible on small (mobile) only</div>
-<div class="visible-medium"><strong>.visible-medium</strong> - Visible on medium (tablets) only</div>
-<div class="visible-mediumup"><strong>.visible-mediumup</strong> - Visible on medium (tablets) and up</div>
-<div class="visible-large"><strong>.visible-large</strong> - Visible on large (desktop) only</div>
+	<div class="visible-small"><strong>.visible-small</strong> - Visible on small (mobile) only</div>
+	<div class="visible-medium"><strong>.visible-medium</strong> - Visible on medium (tablets) only</div>
+	<div class="visible-mediumup"><strong>.visible-mediumup</strong> - Visible on medium (tablets) and up</div>
+	<div class="visible-large"><strong>.visible-large</strong> - Visible on large (desktop) only</div>
 
 <?php include 'partials/footer.php'; ?>
