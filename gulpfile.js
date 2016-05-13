a2dac0c8151b679bcc4b5bd8d83e6e90415d1248
Mike Crantea <mihai2u@gmail.com>
2016-05-13 16:45:57 +0300

    Spring clean-up.
    Added post-css and autoprefixer + stylelint.
    dropped bundle install preinstall (ruby not needed).
    dropped CSS map (not used it much anyways and increases compilation
    time)

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,82 @@
-var gulp = require('gulp'),
-		minifyCSS = require('gulp-cssnano'),
-		watch = require('gulp-watch'),
-		plumber = require('gulp-plumber'),
-		sass = require('gulp-sass'),
-		livereload = require('gulp-livereload'),
-		notify = require('gulp-notify');
+var gulp = require('gulp');
+var minify = require('gulp-cssnano');
+var stylelint = require( 'stylelint' );
+var watch = require('gulp-watch');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var flatten = require( 'gulp-flatten' );
+var livereload = require('gulp-livereload');
+var notify = require('gulp-notify');
+var postcss = require( 'gulp-postcss' );
+var autoprefixer = require( 'autoprefixer' );
+var reporter = require( 'postcss-reporter' );
+var scss = require( 'postcss-scss' );
 
-gulp.task('default', function() {
+gulp.task('default', ['sass-watch'], function() {
 	gulp.watch('./scss/**/*.scss', ['sass-watch']);
 });
 
 gulp.task('sass-watch', function() {
 	return gulp.src('./scss/**/*.scss')
 		.pipe(plumber({
-        handleError: function (err) {
-            console.log(err);
-            this.emit('end');
-        }
-    }))
+				handleError: function (err) {
+						console.log(err);
+						this.emit('end');
+				}
+		}))
+		.pipe( postcss( [
+			stylelint( {
+				'extends': 'stylelint-config-standard',
+				'rules': {
+					'indentation': 'tab',
+					'number-leading-zero': null
+				}
+			} ),
+			reporter( { clearMessages: true } )
+		], {
+			syntax: scss
+		} ) )
 		.pipe(sass({
 				includePaths: [
 					'bower_components/normalize.scss/sass',
 					'bower_components/support-for/sass'
 				],
-		    sourcemap: true,
-		    sourcemapPath: '../scss'
-		  }))
+				outputStyle: 'expanded'
+			}).on( 'error', sass.logError ))
+		.pipe( flatten() )
+		.pipe( postcss( [
+			autoprefixer( {
+				browsers: ['last 2 versions'],
+				cascade: true,
+				remove: true
+			 } )
+		] ) )
 		.pipe(gulp.dest('stylesheets'))
 		.pipe(notify("Compiled <%= file.relative %>"))
-    .pipe(livereload());
+		.pipe(livereload());
 });
 
 gulp.task('production', function() {
 	gulp.src('./scss/*.scss')
-  .pipe(sass({
-  	includePaths: [
+	.pipe(sass({
+		includePaths: [
 			'bower_components/normalize.scss/sass',
 			'bower_components/support-for/sass'
 		],
-  	sourcemap: false
-  }))
-  .pipe(minifyCSS())
-  .pipe(gulp.dest('stylesheets'));
+		sourcemap: false
+	}))
+	.pipe( flatten() )
+	.pipe( postcss( [
+		autoprefixer( {
+			browsers: ['last 2 versions'],
+			cascade: true,
+			remove: true
+		 } )
+	] ) )
+	.pipe(minify({
+		discardComments: {
+				removeAll: true
+			}
+		}))
+	.pipe(gulp.dest('stylesheets'));
 });
